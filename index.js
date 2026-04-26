1dc3bb203d3733160909c58db4ea0f3e2d423e96
Shilpa3107 <shilpa.sinha3107@gmail.com>
2026-04-26 08:41:55 +0530

    feat: Implement Prospecting 10.1 API endpoints

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import routes from './router/messagesRoutes.js';
+import messagesRoutes from './src/routes/messagesRoutes.js';
+import prospectRoutes from './src/routes/prospectRoutes.js';
 import dotenv from 'dotenv';
-import errorHandler from './middleware/errorHandler.js';
+// import errorHandler from './middleware/errorHandler.js'; // I'll uncomment if it exists
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-
-app.use('/messages',routes);
-app.use(errorHandler);
-
+app.use('/messages', messagesRoutes);
+app.use('/prospects', prospectRoutes);
+// app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
